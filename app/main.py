285cfd47365583f103e2c8636a500cb881702ea7
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2025-01-05 22:45:51 +1100

    REFL

--- app/main.py
+++ app/main.py
@@ -3,11 +3,15 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush
+        # Wait for user input
+        command=input()
+        sys.stdout.write(f"{command}: command not found\n")
 
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
+
+    
 
 
 if __name__ == "__main__":
