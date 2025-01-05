cbfaaa32dc3acde0b23c2d37811a2b351987889d
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2025-01-05 23:06:03 +1100

    type functionality

--- app/main.py
+++ app/main.py
@@ -1,18 +1,40 @@
 import sys
 
+VALID_COMMANDS = ["echo", "exit", "type"]
 
 def main():
     # Uncomment this block to pass the first stage
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush
+
+        # Initialisation
+        command, val= ("","")
+
         # Wait for user input
-        command=input()
-        if command=="exit 0":
+        command_input = input()
+
+        # If there is a command, break it up into command and value
+        if " " in command_input:
+            command, val= command_input.split()
+        else:
+            command = command_input
+
+        # Exit command
+        if command=="exit" and val=="0":
             return
         
-        if command[:4]=="echo":
-            print(command[5:])
+        
+        # echo command: print input
+        if command=="echo":
+            print(val)
+
+        # type command: check type of input
+        elif command=="type":
+            if val in VALID_COMMANDS:
+                print(f"{val} is a shell builtin")
+            else:
+                print(f"{val}: not found")
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
