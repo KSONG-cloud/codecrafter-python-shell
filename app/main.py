0f4a86dbc1deb03962059601b48f183a6abe538c
KSONG-cloud <76906456+KSONG-cloud@users.noreply.github.com>
2025-01-07 16:02:52 +1100

    debugging stuff 2

--- app/main.py
+++ app/main.py
@@ -66,13 +66,13 @@ def main():
         # and has the same limitations. If the command generates any 
         # output, it is sent to the interpreter’s standard output stream. 
         elif os.path.isfile(command):
-            print("BRUH")
             os.system(f"{command} {val}")
             continue
         else:
             for path in paths:
                 if os.path.isfile(f"{path}/{command}"):
                     print("HEY")
+                    print(path)
                     os.system(f"{path}/{command} {val}")
                     continue
                       
